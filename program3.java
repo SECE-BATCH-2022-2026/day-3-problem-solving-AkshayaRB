d49b6f7e82255d3485aa88253b5f1c1078b96b03
AkshayaRB <akshayaradhakrishnan09@gmail.com>
2025-12-05 14:30:33 +0530

    adding program3.java

---
+++ program3.java
@@ -0,0 +1,38 @@
+// triplet problem
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+
+    public static boolean hasTriplet(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count occurrences
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            // If any number reaches count 3 → triplet exists
+            if (map.get(num) == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (hasTriplet(arr)) {
+            System.out.println("Triplet Found");
+        } else {
+            System.out.println("No Triplet");
+        }
+    }
+}
