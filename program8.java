6018159ba47db3c8270d6000485073f7262b26b4
AkshayaRB <akshayaradhakrishnan09@gmail.com>
2025-12-10 09:55:58 +0530

    adding program8.java

---
+++ program8.java
@@ -0,0 +1,15 @@
+java.util.*;
+class Main{
+	public static void main(Stringn args[]){
+		Scanner sc= new Scanner(System.in);
+		String inp = sc.nextLine();
+		System.out.println(UserMainCode.regularExpression(inp));
+	}
+class UserMainCode{
+	public static booolean regularExpression(String inp){
+		return inp.matches("[a-zA-Z0-9][R][0-9][a-zA-Z0-9]");
+		
+	}
+}	
+	
+}
